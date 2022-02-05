85e8535d558be7c95bf1a910c45c5e8212d72f93
Bebe Jane <bjornthief@gmail.com>
2022-02-05 08:31:09 +0100

    timing

--- pages/api/catalogue/index.js
+++ pages/api/catalogue/index.js
@@ -6,8 +6,9 @@ import getConfig from 'next/config'
 const { serverRuntimeConfig } = getConfig()
 
 export default async function catalogue(req, res) {
-  
-  const id = req.body?.entity?.id ? req.body.entity.id : req.query.id ? req.query.id[0] : null;
+  console.time('pupeteer')
+  const isWebhook = (req.body?.entity?.id)
+  const id = isWebhook ? req.body.entity.id : req.query.id ? req.query.id[0] : null;
   const url = `${process.env.NEXT_PUBLIC_SITE_URL}/catalogue${id ? `/${id}` : ''}`;
   const browser = await puppeteer.launch(process.env.NODE_ENV === 'production' ? { args: chrome.args, executablePath: await chrome.executablePath, headless: chrome.headless } : {});
   const pdfFilePath = `${serverRuntimeConfig.TEMP_DIR}/${id}.pdf`
@@ -26,23 +27,24 @@ export default async function catalogue(req, res) {
       right: '0px'
     },
   });
+  
   await page.close();
   await browser.close();
+  console.timeEnd('pupeteer')
+  console.time('upload')
   fs.writeFileSync(pdfFilePath, pdfBuffer)
 
   const datoClient = new SiteClient(process.env.CMS_API_TOKEN);
   const record = await datoClient.items.all({filter: { type: 'product', fields: { id: {eq: id}}}});
-  
-  
 
-  if(req.body?.entity?.id){
-    console.log('uploading pdf...')
+  if(isWebhook){
+    console.log('Uploading pdf...')
     await datoClient.uploads.update(record[0].pdfFile.uploadId, {path: await datoClient.createUploadPath(pdfFilePath)});
-    console.log('done')
+    console.timeEnd('upload')
     return res.json({success:true})
   }
-
-  res.setHeader('Content-Type', 'application/pdf'); //res.setHeader('Content-Disposition', `attachment; filename="Örsjö - Catalogue 2022.pdf"`)
-  res.send(pdfBuffer)
-  
+  else{
+    res.setHeader('Content-Type', 'application/pdf'); //res.setHeader('Content-Disposition', `attachment; filename="Örsjö - Catalogue 2022.pdf"`)
+    res.send(pdfBuffer)
+  }
 }
