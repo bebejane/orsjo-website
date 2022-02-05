c9011b3a014aaf6b3a0bc9d32148be66ded67ee1
Bebe Jane <bjornthief@gmail.com>
2022-02-05 09:06:03 +0100

    test lamda

--- pages/api/catalogue/index.js
+++ pages/api/catalogue/index.js
@@ -1,22 +1,39 @@
 import puppeteer from "puppeteer";
-import chrome from 'chrome-aws-lambda';
+import chromium from 'chrome-aws-lambda';
 import fs from 'fs';
 import { SiteClient } from 'datocms-client';
 import getConfig from 'next/config'
 const { serverRuntimeConfig } = getConfig()
+
+let browserWSEndpoint
 
 export default async function catalogue(req, res) {
   console.time('pupeteer')
   const isWebhook = (req.body?.entity?.id)
   const id = isWebhook ? req.body.entity.id : req.query.id ? req.query.id[0] : null;
   const url = `${process.env.NEXT_PUBLIC_SITE_URL}/catalogue${id ? `/${id}` : ''}`;
-  const browser = await puppeteer.launch(process.env.NODE_ENV === 'production' ? { args: chrome.args, executablePath: await chrome.executablePath, headless: chrome.headless } : {});
+  let browser, page, pdfBuffer;
+
+  try {
+    if (browserWSEndpoint) {
+      browser = await chromium.puppeteer.connect({ browserWSEndpoint })
+    }
+
+    if (!browser || !browser.isConnected()) {
+      browser = await puppeteer.launch(process.env.NODE_ENV === 'production' ? { args: chromium.args, executablePath: await chromium.executablePath, headless: chromium.headless } : {});
+      // Keep blank page open
+      //page = await browser.newPage()
+      browserWSEndpoint = browser.wsEndpoint()
+    }
+
+    page = await browser.newPage()
+    //const browser = await puppeteer.launch(process.env.NODE_ENV === 'production' ? { args: chromium.args, executablePath: await chromium.executablePath, headless: chromium.headless } : {});
   const pdfFilePath = `${serverRuntimeConfig.TEMP_DIR}/${id}.pdf`
 
-  const page = await browser.newPage(); 
+  //const page = await browser.newPage(); 
   await page.goto(url, {waitUntil:'networkidle0'});
   
-  const pdfBuffer = await page.pdf({
+  pdfBuffer = await page.pdf({
     format: 'A4',
     printBackground: true,
     preferCSSPageSize: true,
@@ -27,16 +44,15 @@ export default async function catalogue(req, res) {
       right: '0px'
     },
   });
+  await page.close()
+  await browser.disconnect()
   
-  await page.close();
-  await browser.close();
   console.timeEnd('pupeteer')
   console.time('upload')
   fs.writeFileSync(pdfFilePath, pdfBuffer)
 
   const datoClient = new SiteClient(process.env.CMS_API_TOKEN);
   const record = await datoClient.items.all({filter: { type: 'product', fields: { id: {eq: id}}}});
-
   if(isWebhook){
     console.log('Uploading pdf...')
     await datoClient.uploads.update(record[0].pdfFile.uploadId, {path: await datoClient.createUploadPath(pdfFilePath)});
@@ -47,4 +63,11 @@ export default async function catalogue(req, res) {
     res.setHeader('Content-Type', 'application/pdf'); //res.setHeader('Content-Disposition', `attachment; filename="Örsjö - Catalogue 2022.pdf"`)
     res.send(pdfBuffer)
   }
+  
+
+  }catch(err){
+    console.error(err)
+  
+  }
+ 
 }
