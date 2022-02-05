06de6f727ed751f6f5c3dd2e89b1ecc053de2461
Bebe Jane <bjornthief@gmail.com>
2022-02-05 16:33:49 +0100

    separate single multi pdf

--- pages/api/catalogue/[...id].js
+++ pages/api/catalogue/[...id].js
@@ -1,3 +1,38 @@
-import catalogue from ".";
+import { SiteClient } from 'datocms-client';
+import { isAuthorized, generatePDF } from "./";
 
-export default catalogue;
+export default async function productSheet(req, res) {
+
+  const isWebhook = (req.body?.entity?.id)
+
+  if(isWebhook && !isAuthorized(req, res)) return
+
+  const id = isWebhook ? req.body.entity.id : req.query.id ? req.query.id[0] : null;
+  const title = id
+  const url = `${process.env.NEXT_PUBLIC_SITE_URL}/catalogue/${id}`
+  const {buffer, filePath} = await generatePDF(url, title);
+
+  if(isWebhook && id){
+    
+    console.time('upload')
+    const datoClient = new SiteClient(process.env.CMS_API_TOKEN);
+    const path = await datoClient.createUploadPath(filePath)
+    const record = await datoClient.items.all({filter: { type: 'product', fields: { id: {eq: id}}}});
+
+    if(record && record.length === 1 && record[0].pdfFile) 
+      await datoClient.uploads.update(record[0].pdfFile.uploadId, {path});
+    else{
+      const upload = await datoClient.uploads.create({path, tags:['product-pdf']})
+      await datoClient.items.update(id, {pdfFile:{upload_id:upload.id}})
+    }
+    res.json({success:true})
+    console.timeEnd('upload')
+  }
+  else {
+    res.setHeader('Content-Type', 'application/pdf');
+     //res.setHeader('Content-Disposition', `attachment; filename="Örsjö - Catalogue 2022.pdf"`)
+    res.send(buffer)
+  }
+  
+  console.timeEnd(`generate pdf ${title}`)
+}
--- pages/api/catalogue/index.js
+++ pages/api/catalogue/index.js
@@ -1,6 +1,78 @@
-import puppeteer, { ConsoleMessage } from "puppeteer";
+import puppeteer from "puppeteer";
 import chrome from 'chrome-aws-lambda';
 import { SiteClient } from 'datocms-client';
+
+export default async function priceList(req, res) {
+
+  const isWebhook = (req.body?.entity)
+
+  if(isWebhook && !isAuthorized(req, res)) return
+
+  const title = 'price-list'
+  const url = `${process.env.NEXT_PUBLIC_SITE_URL}/catalogue`;
+  const {buffer, filePath} = await generatePDF(url, title);
+
+  if(isWebhook){
+    
+    console.time('upload')
+    const datoClient = new SiteClient(process.env.CMS_API_TOKEN);
+    const path = await datoClient.createUploadPath(filePath)
+    const record = await datoClient.items.all({filter: { type: 'pricelist'}});
+    
+    console.log('uploading pdf...')
+
+    if(record && record.length === 1 && record[0].pdfFile) 
+      await datoClient.uploads.update(record[0].pdfFile.uploadId, {path});
+    else{
+      const upload = await datoClient.uploads.create({path, tags:['pricelist-pdf']})
+      await datoClient.items.update(id, {pdfFile:{upload_id:upload.id}})
+    }
+    res.json({success:true})
+    console.timeEnd('upload')
+  }
+  else {
+    res.setHeader('Content-Type', 'application/pdf');
+     //res.setHeader('Content-Disposition', `attachment; filename="Örsjö - Catalogue 2022.pdf"`)
+    res.send(buffer)
+  }
+  console.timeEnd(`generate pdf ${title}`)
+}
+
+const generatePDF = async (url, title) => {
+
+  console.time(`generate pdf ${title}`)
+  console.time('pupeteer')
+  
+  const filePath = `/tmp/${title}.pdf`;
+  const browser = url.includes('heroku') ? await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']}) : await puppeteer.launch(process.env.NODE_ENV === 'production' ? { args: chrome.args, executablePath: await chrome.executablePath, headless: chrome.headless } : {});
+  const page = await browser.newPage(); 
+
+  console.timeEnd('pupeteer')
+
+  console.time('loadpage')
+  await page.goto(url, {waitUntil:'networkidle0'});
+  console.timeEnd('loadpage')
+
+  console.time('generate')
+  const buffer = await page.pdf({
+    path: `/tmp/${title}.pdf`,
+    format: 'A4',
+    printBackground: true,
+    preferCSSPageSize: true,
+    margin: {
+      top: '0px',
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    },
+  });
+  console.timeEnd('generate')
+
+  await page.close();
+  await browser.close();
+  console.timeEnd(`generate pdf ${title}`)
+  return {buffer, filePath}
+}
 
 const isAuthorized = (req, res) => {
   
@@ -14,74 +86,9 @@ const isAuthorized = (req, res) => {
   }
 
   const auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
-  
   const username = auth[0];
   const password = auth[1];
-
   return username === process.env.DATOCMS_WEBHOOK_USERNAME && password === process.env.DATOCMS_WEBHOOK_PASSWORD
 }
 
-export default async function catalogue(req, res) {
-
-  const isWebhook = (req.body?.entity?.id)
-
-  if(isWebhook && !isAuthorized(req, res)) return
-
-  const id = isWebhook ? req.body.entity.id : req.query.id ? req.query.id[0] : null;
-  const title = id || 'price-list'
-  const url = `${process.env.NEXT_PUBLIC_SITE_URL}/catalogue${id ? `/${id}` : ''}`;
-  const pdfFilePath = `/tmp/${title}.pdf`
-
-  console.time(`generate pdf ${title}`)
-  console.time('pupeteer')
-
-  const browser = url.includes('heroku') ? await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']}) : await puppeteer.launch(process.env.NODE_ENV === 'production' ? { args: chrome.args, executablePath: await chrome.executablePath, headless: chrome.headless } : {});
-  const page = await browser.newPage(); 
-  console.timeEnd('pupeteer')
-
-  console.time('loadpage')
-  await page.goto(url, {waitUntil:'networkidle0'});
-  console.timeEnd('loadpage')
-
-  const pdfBuffer = await page.pdf({
-    path: isWebhook ? pdfFilePath : undefined,
-    format: 'A4',
-    printBackground: true,
-    preferCSSPageSize: true,
-    margin: {
-      top: '0px',
-      bottom: '0px',
-      left: '0px',
-      right: '0px'
-    },
-  });
-  
-  await page.close();
-  await browser.close();
-  
-  if(isWebhook && id){
-    
-    console.time('upload')
-    const datoClient = new SiteClient(process.env.CMS_API_TOKEN);
-    const path = await datoClient.createUploadPath(pdfFilePath)
-    const record = await datoClient.items.all({filter: { type: 'product', fields: { id: {eq: id}}}});
-    
-    console.log('uploading pdf...')
-
-    if(record && record.length === 1 && record[0].pdfFile) 
-      await datoClient.uploads.update(record[0].pdfFile.uploadId, {path});
-    else{
-      const upload = await datoClient.uploads.create({path, tags:['product-pdf']})
-      await datoClient.items.update(id, {pdfFile:{upload_id:upload.id}})
-    }
-    res.json({success:true})
-    console.timeEnd('upload')
-  }
-  else {
-    res.setHeader('Content-Type', 'application/pdf');
-     //res.setHeader('Content-Disposition', `attachment; filename="Örsjö - Catalogue 2022.pdf"`)
-    res.send(pdfBuffer)
-  }
-  
-  console.timeEnd(`generate pdf ${id}`)
-}
+export { isAuthorized, generatePDF }
