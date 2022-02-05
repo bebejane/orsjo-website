b3acaa0bbe0fc1b5ea580244c41e9ca60041b4e5
Bebe Jane <bjornthief@gmail.com>
2022-02-05 16:09:40 +0100

    revert

--- pages/api/catalogue/index.js
+++ pages/api/catalogue/index.js
@@ -2,6 +2,25 @@ import puppeteer, { ConsoleMessage } from "puppeteer";
 import chrome from 'chrome-aws-lambda';
 import { SiteClient } from 'datocms-client';
 
+const isAuthorized = (req, res) => {
+  
+  const authheader = req.headers.authorization;
+
+  if (!authheader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      res.status(401).send(err)
+      return false
+  }
+
+  const auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+  
+  const username = auth[0];
+  const password = auth[1];
+
+  return username === process.env.DATOCMS_WEBHOOK_USERNAME && password === process.env.DATOCMS_WEBHOOK_PASSWORD
+}
+
 export default async function catalogue(req, res) {
 
   const isWebhook = (req.body?.entity?.id)
@@ -11,9 +30,7 @@ export default async function catalogue(req, res) {
   const id = isWebhook ? req.body.entity.id : req.query.id ? req.query.id[0] : null;
   const title = id || 'price-list'
   const url = `${process.env.NEXT_PUBLIC_SITE_URL}/catalogue${id ? `/${id}` : ''}`;
-  const urlFull = `${process.env.NEXT_PUBLIC_SITE_URL}/catalogue`;
   const pdfFilePath = `/tmp/${title}.pdf`
-  const pdfFileFullPath = `/tmp/price-list.pdf`
 
   console.time(`generate pdf ${title}`)
   console.time('pupeteer')
@@ -39,24 +56,10 @@ export default async function catalogue(req, res) {
     },
   });
   
-  
-  await page.goto(urlFull, {waitUntil:'networkidle0'});
-  const pdfBufferFull = await page.pdf({
-    path: isWebhook ? pdfFileFullPath : undefined,
-    format: 'A4',
-    printBackground: true,
-    preferCSSPageSize: true,
-    margin: {
-      top: '0px',
-      bottom: '0px',
-      left: '0px',
-      right: '0px'
-    },
-  });
   await page.close();
   await browser.close();
   
-  if(isWebhook){
+  if(isWebhook && id){
     
     console.time('upload')
     const datoClient = new SiteClient(process.env.CMS_API_TOKEN);
@@ -71,20 +74,6 @@ export default async function catalogue(req, res) {
       const upload = await datoClient.uploads.create({path, tags:['product-pdf']})
       await datoClient.items.update(id, {pdfFile:{upload_id:upload.id}})
     }
-
-
-    const pathFull = await datoClient.createUploadPath(pdfFileFullPath)
-    const recordFull = await datoClient.items.all({filter: { type: 'pricelist'}});
-    
-    console.log('uploading full pdf...')
-
-    if(recordFull && recordFull.length === 1 && recordFull[0].pdfFile) 
-      await datoClient.uploads.update(recordFull[0].pdfFile.uploadId, {path:pathFull});
-    else{
-      const uploadFull = await datoClient.uploads.create({path:pathFull, tags:['pricelist-pdf']})
-      await datoClient.items.update(id, {pdfFile:{upload_id:uploadFull.id}})
-    }
-
     res.json({success:true})
     console.timeEnd('upload')
   }
@@ -94,24 +83,5 @@ export default async function catalogue(req, res) {
     res.send(pdfBuffer)
   }
   
-  console.timeEnd(`generate pdf ${title}`)
-}
-
-const isAuthorized = (req, res) => {
-  
-  const authheader = req.headers.authorization;
-
-  if (!authheader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      res.status(401).send(err)
-      return false
-  }
-
-  const auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
-  
-  const username = auth[0];
-  const password = auth[1];
-
-  return username === process.env.DATOCMS_WEBHOOK_USERNAME && password === process.env.DATOCMS_WEBHOOK_PASSWORD
+  console.timeEnd(`generate pdf ${id}`)
 }
