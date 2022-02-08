9b0cb12fbfd8454a3ebdf08bc67b36c1ff190f7b
Bebe Jane <bjornthief@gmail.com>
2022-02-08 15:18:02 +0100

    single product sheet

--- pages/catalogue/[...id].js
+++ pages/catalogue/[...id].js
@@ -1,5 +1,5 @@
 import styles from './index.module.scss'
-import Catalogue from '/components/catalogue/Catalogue';
+import ProductSheet from '/components/catalogue/ProductSheet';
 import { apiQuery } from "/lib/dato/api";
 import { GetProduct } from "/graphql"
 
@@ -7,12 +7,13 @@ export default function Home(props){
 	const { product } = props
 	return (
 		<div className={styles.container}>
-      <Catalogue products={[product]}/>
+      <ProductSheet product={product}/>
 		</div>
 	)
 }
 
 export const getServerSideProps = async (context) => {
+
   const id = context.params.id[0];
 	const locale = context.query.locale || 'en';
 	const { product } = await apiQuery(GetProduct, {id, locale});
@@ -25,15 +26,3 @@ export const getServerSideProps = async (context) => {
 		}
 	}
 }
-
-/*
-export async function getStaticPaths() {
-  const { products } = await apiQuery(GetProducts);
-	const paths = []
-  products.forEach(({id}) => paths.push({ params: { id: [id] }}));
-	return {
-		paths:paths,
-		fallback: 'blocking',
-	};
-}
-*/
