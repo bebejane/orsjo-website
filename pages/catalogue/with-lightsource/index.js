e195df98c95b45ba5721f5fff8a497a64664d0f9
Bebe Jane <bjornthief@gmail.com>
2022-02-24 14:47:40 +0100

    clean

--- pages/catalogue/with-lightsource/index.js
+++ pages/catalogue/with-lightsource/index.js
@@ -3,6 +3,9 @@ import CatalogueLight from '/components/catalogue/CatalogueLight';
 import { apiQuery, intlQuery } from "/lib/dato/api";
 import { GetProducts } from "/graphql"
 
+const hardWiredModelNameId = 107174981
+const fixedMountingId = 107174756
+
 export default function catalogueLight({products}) {
 	
 	return (
@@ -12,21 +15,16 @@ export default function catalogueLight({products}) {
 	)
 }
 
-const hardWiredModelNameId = 107174981
-const fixedMountingId = 107174756
-
-export const getServerSideProps = async ({ locale }) => {
-	
+export const getServerSideProps = async ({ locale }) => {	
 	let { products } = await apiQuery(GetProducts, { locale });
 	if (!products) return { notFound: true }
 
-	console.log(products.length, products.reduce((acc, p) => acc + p.models.length, 0))
-	
 	// Filter out model name containing hard wired and products with mounting that is fixed
 	products.forEach((p, idx) => products[idx].models = p.models.filter(m => !(m.name?.id == hardWiredModelNameId)))
 	products = products.filter(p => p.models.length > 0 && !(p.mounting?.id == fixedMountingId))
 
-	console.log(products.length, products.reduce((acc, p) => acc + p.models.length, 0))
+	//console.log(products.length, products.reduce((acc, p) => acc + p.models.length, 0))
+
 	return {
 		props: {
 			products,
