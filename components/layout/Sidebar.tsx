3dd9ce902bb65cd035bb05ac030bbe1356033526
konst-teknik <information-konst-teknik.se>
2022-06-16 13:10:30 +0200

    Layout

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -7,7 +7,15 @@ export default function Sidebar() {
 
 	return (
 		<aside className={styles.sidebar}>
-      sidebar
-    </aside>
+			<h3>Subheader</h3>
+			<nav>
+				<ul>
+					<li>Menu item 1</li>
+					<li>Menu item 2</li>
+					<li>Menu item 3</li>
+					<li>Menu item 4</li>
+				</ul>
+			</nav>
+		</aside>
 	)
 }
