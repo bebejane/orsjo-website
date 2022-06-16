0b7bb0a23e973a3c80fc71846adcba24b5111eb4
konst-teknik <information-konst-teknik.se>
2022-06-16 17:46:09 +0200

    Fix

--- components/blocks/ImageGalleryBlock.tsx
+++ components/blocks/ImageGalleryBlock.tsx
@@ -5,16 +5,14 @@ import { Image } from 'react-datocms'
 type ImageGalleryBlockProps = { data: ImageGallery }
 
 export default function ImageGalleryBlock({ data: { gallery } }: ImageGalleryBlockProps) {
-	
+
 	return (
 		<div className={styles.imageGallery}>
-			{gallery.map(({responsiveImage}, idx) => 
+			{gallery.map(({ responsiveImage }, idx) =>
 				<figure key={idx}>
-					<Image 
-						data={responsiveImage} 
-						className={styles.image} 
-						layout={'fill'} 
-						objectFit={'contain'}
+					<Image
+						data={responsiveImage}
+						className={styles.image}
 					/>
 				</figure>
 			)}
