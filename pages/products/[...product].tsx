3e0e65159cac2c0b9d35d24e4fc82612d4ab42aa
Bebe Jane <bjornthief@gmail.com>
2022-06-16 13:49:03 +0200

    product page

---
+++ pages/products/[...product].tsx
@@ -0,0 +1,77 @@
+import styles from './Product.module.scss'
+import { GetProducts, GetProduct } from '/graphql'
+import { apiQuery } from '/lib/dato/api'
+import { withGlobalProps } from '/lib/hoc'
+import { List, ListItem} from '/components'
+import { Image } from 'react-datocms'
+import Markdown from '/lib/dato/components/Markdown'
+
+type ProductProps = { product: Product };
+
+export default function Product({product} : ProductProps){
+console.log(product)
+	return (
+		<>
+			<section className={styles.product}>
+				<div className={styles.image}>
+					<Image data={product.image?.responsiveImage} layout={'fill'} objectFit={'contain'}/>
+					<div className={styles.overlay}>
+						<div className={styles.text}>
+							<h1 className={styles.title}>
+								{product.title}
+							</h1>
+							<h1 className={styles.designer}>
+								{product.designer?.name}
+							</h1>
+							<h3 className={styles.type}>
+								type
+							</h3>
+						</div>
+					</div>
+				</div>
+				<Markdown>{product.description}</Markdown>
+			</section>
+			<section className={styles.details}>
+				<List initial={0}>
+					<ListItem title={'Colors'}>
+						<div className={styles.colorImages}>
+							{product.colorImages.map((image) =>
+								<Image data={image.responsiveImage} className={styles.image}/>
+							)}
+						</div>
+					</ListItem>
+					<ListItem title={'Specifications'}>
+						specs content
+					</ListItem>
+					<ListItem title={'Downloads'}>
+						downloads content
+					</ListItem>
+				</List>	
+			</section>
+		</>
+	)
+}
+
+export async function getStaticPaths(context) {
+	const { products } = await apiQuery(GetProducts)
+	const paths = products.map(({ slug }) => ({ params: { product: [slug] } }))
+	return {
+		paths,
+		fallback: 'blocking'
+	}
+}
+
+export const getStaticProps = withGlobalProps({ model: 'product' }, async ({ props, context, revalidate }) => {
+	const { product } = await apiQuery(GetProduct, {variables: { slug: context.params.product[0] }})
+
+	if (!product) 
+		return { notFound: true }
+
+	return {
+		props: {
+			...props,
+			product
+		},
+		revalidate
+	};
+});
