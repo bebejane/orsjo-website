539b9630f3cabc1646b8899caa914e2bbe188686
Bebe Jane <bjornthief@gmail.com>
2022-06-17 11:57:29 +0200

    fix

--- components/layout/Content.tsx
+++ components/layout/Content.tsx
@@ -10,14 +10,13 @@ export type ContentProps = { children: React.ReactNode }
 export default function Content({ children }: ContentProps) {
 
 	const router = useRouter()
-	const [currentSection,setCurrentSection, setSections] = useStore((state) => [state.currentSection, state.setCurrentSection, state.setSections], shallow);
-	const [pageHeight, setPageHeight] = useState(0);
-	const { isScrolling, scrolledPosition, documentHeight } = useScrollInfo()
+	const [setCurrentSection, setSections] = useStore((state) => [state.setCurrentSection, state.setSections], shallow);
+	const { scrolledPosition, documentHeight } = useScrollInfo()
 
 	useEffect(()=>{
 		const sections = document.querySelectorAll('section[id]')
 		setSections(sections.length ? Array.from(sections).map((s)  => s.title || s.id) : [])
-	}, [])
+	}, [router.asPath])
 
 	useEffect(()=>{
 		const sections = document.querySelectorAll('section[id]')
