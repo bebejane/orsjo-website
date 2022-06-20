f5d5826b6f382e080d3cb739b7bb4933059c5c60
Bebe Jane <bjornthief@gmail.com>
2022-06-20 07:59:51 +0200

    fix

--- @types/modules.d.ts
+++ @types/modules.d.ts
@@ -1,3 +1,9 @@
+declare module '*.gql' {
+  import { DocumentNode } from 'graphql';
+  const value: DocumentNode;
+  export = schema;
+}
+
 declare module 'markdown-truncate' {
   function truncateMarkdown(string, {limit:number, ellipsis:booelan}): string;
   export = truncateMarkdown;
