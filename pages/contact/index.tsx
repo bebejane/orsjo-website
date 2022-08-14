ddeeebdcda069c2c1ec3bbd106beebf69e0cc196
konst-teknik <information-konst-teknik.se>
2022-08-14 12:16:05 +0200

    Contanc

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,10 +1,10 @@
 import styles from './index.module.scss'
-import { 
-	GetContactDocument, 
-	GetAllResellersDocument, 
-	GetAllStaffsDocument, 
-	GetAllShowroomsDocument, 
-	GetAllDistributorsDocument 
+import {
+	GetContactDocument,
+	GetAllResellersDocument,
+	GetAllStaffsDocument,
+	GetAllShowroomsDocument,
+	GetAllDistributorsDocument
 } from '/graphql';
 import { withGlobalProps } from "/lib/hoc";
 import { Image } from 'react-datocms'
@@ -24,55 +24,61 @@ export default function Contact({ contact, resellers, staffs, showrooms, distrib
 
 	const resellesByCountry = {}
 	resellers.forEach((r, i) => {
-		if(!resellesByCountry[r.country.id]) 
-			resellesByCountry[r.country.id] = {resellers:[], country:r.country.name}
+		if (!resellesByCountry[r.country.id])
+			resellesByCountry[r.country.id] = { resellers: [], country: r.country.name }
 		resellesByCountry[r.country.id].resellers.push(r)
 	})
-	
+
 	return (
 		<div className={styles.contact}>
 			<section {...sectionId('Information')} className={styles.informationSection}>
-				
-					<div className={styles.info}>
-						<h1>{contact.title}</h1>
-						<Markdown>
-							{contact.intro}
-						</Markdown>
-						<div className={styles.getintouch}>
-							<div className={styles.visit}>
-								<h2>Visit</h2>
+
+				<div className={styles.info}>
+					<h1>{contact.title}</h1>
+					<p className="white bodyWeight">
+						{contact.intro}
+					</p>
+					<div className={styles.getintouch}>
+						<div className={styles.visit}>
+							<p className="white medium noMargin">Visit</p>
+							<div className="medium">
 								<Markdown>{contact.address}</Markdown>
 							</div>
-							<div className={styles.reachout}>
-								<h2>Reach out</h2>
-								<a href={`tel://${contact.phone}`}>{contact.phone}</a><br/>
-								<a href={`mailto:${contact.email}`}>{contact.email}</a>
-							</div>
 						</div>
-						<button>Contact Us</button>
+						<div className={styles.reachout}>
+							<p className="white medium noMargin">Reach out</p>
+							<p className="medium">
+								<a href={`tel://${contact.phone}`}>{contact.phone}</a><br />
+								<a href={`mailto:${contact.email}`}>{contact.email}</a>
+							</p>
+						</div>
 					</div>
-					<div className={styles.image}>
-						<Image data={contact.image.responsiveImage}/>
-					</div>
+					<button>Contact Us</button>
+				</div>
+				<div className={styles.image}>
+					<Image data={contact.image.responsiveImage} />
+				</div>
 			</section>
 			<section {...sectionId('Staff')} className={styles.staffSection}>
 				<h1>Staff</h1>
 				<div className={styles.staff}>
-				{staffs.map(({name, role, phone, email, image}, idx) => 
-					<div key={idx} className={styles.employee}>
-						<div className={styles.image}>
-							<Image data={image.responsiveImage}/>
+					{staffs.map(({ name, role, phone, email, image }, idx) =>
+						<div key={idx} className={styles.employee}>
+							<div className={styles.image}>
+								<Image data={image.responsiveImage} />
+							</div>
+							<div className={styles.name}>
+								<p className="medium white noMargin">
+									{name}</p>
+							</div>
+							<div className={styles.image}>
+								<p className="medium">							{role}<br />
+									<a href={`tel://${phone}`}>{phone}</a><br />
+									<a href={`mailto:${email}`}>{email}</a>
+								</p>
+							</div>
 						</div>
-						<div className={styles.name}>
-							{name}
-						</div>
-						<div className={styles.image}>
-							{role}<br/>
-							<a href={`tel://${phone}`}>{phone}</a><br/>
-							<a href={`mailto:${email}`}>{email}</a>
-						</div>
-					</div>
-				)}
+					)}
 				</div>
 			</section>
 			<section {...sectionId('Showrooms')} data-dark="1" className={styles.showroomsSection}>
@@ -80,67 +86,74 @@ export default function Contact({ contact, resellers, staffs, showrooms, distrib
 				<Markdown className={styles.intro}>
 					The best way to experience our products is to see them in real life, so just reach out and book an appointment.
 				</Markdown>
-				{showrooms.map(({image, city, address, additional}, idx) => 
-					<div key={idx} className={styles.showroom}>
-						<div className={styles.left}>
-							{image?.responsiveImage && 
-								<Image 
-									data={image.responsiveImage} 
-									className={styles.image} 
-									layout={'responsive'} 
-									objectFit={'contain'}
-								/>
-							}
-						</div>
-						<div className={styles.right}>
-							<h2>{city}</h2>
-							<Markdown className={styles.text}>{address}</Markdown>
-							<Markdown className={styles.text}>{additional}</Markdown>
-						</div>
-					</div>
-				)}
+				<ul>
+					{showrooms.map(({ image, city, address, additional }, idx) =>
+						<li key={idx} className={styles.showroom}>
+							<div className={styles.left}>
+								{image?.responsiveImage &&
+									<Image
+										data={image.responsiveImage}
+										className={styles.image}
+										layout={'responsive'}
+										objectFit={'contain'}
+									/>
+								}
+							</div>
+							<div className={styles.right}>
+								<div className="medium">
+									<p className="red">{city}</p>
+									<Markdown className={styles.text}>{address}</Markdown>
+									<Markdown className={styles.text}>{additional}</Markdown>
+								</div>
+							</div>
+						</li>
+					)}
+				</ul>
 			</section>
 			<section {...sectionId('Agents & Distributors')} className={styles.distributorSection}>
-				<h1>Agents & Distributors</h1>
+				<h1 className="white bottomMargin">Agents & Distributors</h1>
 				<div className={styles.distributors}>
-					{distributors.map(({name, address, city, country, email, phone, postalCode, contactName, url}, idx) =>
+					{distributors.map(({ name, address, city, country, email, phone, postalCode, contactName, url }, idx) =>
 						<div key={idx} className={styles.distributor}>
-							<h2>{country.name}</h2>
-							{address && <>{address}<br/></> }
-							{postalCode && <>{postalCode}<br/></> }
-							{city && <>{city}<br/></> }
-							{contactName && <>{contactName}<br/></> }
-							{phone && <>{phone}<br/></> }
-							{email && <><a href={`mailto:${email}`}>{email}</a><br/></> }
-							{url && <a href={url}>{url.replace('https://', '')}</a> }
+							<p className="medium">
+								<span className="white">{country.name}</span><br />
+								{address && <>{address}<br /></>}
+								{postalCode && <>{postalCode}<br /></>}
+								{city && <>{city}<br /></>}
+								{contactName && <>{contactName}<br /></>}
+								{phone && <>{phone}<br /></>}
+								{email && <><a href={`mailto:${email}`}>{email}</a><br /></>}
+								{url && <a href={url}>{url.replace('https://', '')}</a>}
+							</p>
 						</div>
 					)}
 				</div>
 			</section>
 			<section {...sectionId('Retailers')} data-dark="1" className={styles.resellerSection}>
-				<h1>Retailers</h1>
+				<h1 className="red bottomMargin">Retailers</h1>
 				<div className={styles.resellers}>
 					{Object.keys((resellesByCountry)).map((id, idx) => {
-						const {country} = resellesByCountry[id];
+						const { country } = resellesByCountry[id];
 						const items = resellesByCountry[id].resellers;
 						return (
 							<div key={idx} className={styles.country}>
-								<h1>{country}</h1>
+								<h2 className="topMargin">{country}</h2>
 								<div className={styles.wrap}>
-								{items.map(({name, address, postalCode, city, phone, email, url}, ridx) => 
-									<div key={ridx} className={styles.reseller}>
-										<div className={styles.name}>
-											{name}
+									{items.map(({ name, address, postalCode, city, phone, email, url }, ridx) =>
+										<div key={ridx} className={styles.reseller}>
+											<p className="medium">
+												<span className="red">{name}</span><br />
+												{address && <>{address}<br /></>}
+												{postalCode && <>{postalCode}<br /></>}
+												{city && <>{city}<br /></>}
+												{phone && <>{phone}<br /></>}
+												{email && <><a href={`mailto:${email}`}>{email}</a><br /></>}
+												{url && <a href={url}>{url.replace('https://', '')}</a>}
+											</p>
+
 										</div>
-										{address && <>{address}<br/></> }
-										{postalCode && <>{postalCode}<br/></> }
-										{city && <>{city}<br/></> }
-										{phone && <>{phone}<br/></> }
-										{email && <><a href={`mailto:${email}`}>{email}</a><br/></> }
-										{url && <a href={url}>{url.replace('https://', '')}</a> }
-									</div>
-								)}
-								</div>	
+									)}
+								</div>
 							</div>
 						)
 					})}
