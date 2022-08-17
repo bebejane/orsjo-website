edaed32e48a5ab7f376f4594c5577900a32fdd8d
Bebe Jane <bjornthief@gmail.com>
2022-08-17 09:39:11 +0200

    faq page

--- pages/support/faq.tsx
+++ pages/support/faq.tsx
@@ -1,25 +1,32 @@
-import styles from './index.module.scss'
+import styles from './faq.module.scss'
 import { GetAllFaqsDocument } from '/graphql';
 import { withGlobalProps } from "/lib/hoc";
-import Link from 'next/link'
-import { Image } from 'react-datocms'
+import cn from 'classnames'
 import Markdown from '/lib/dato/components/Markdown';
 import { PageLayoutProps } from '/lib/context/layout';
 import { Section } from '/components'
+import { useState } from 'react';
 
 export type FaqsProps = { faqs: FaqRecord[] }
 
 export default function Faqs({ faqs }: FaqsProps) {
+	
+	const [list, setList] = useState({})
 
 	return (
 		<Section className={styles.faq} top={true}>
 			<h1>Faq</h1>
-			{faqs.map(({ question, answer, category: { title } }) =>
-				<>
-					<span>{question}</span>
-					<Markdown>{answer}</Markdown>
-				</>
-			)}
+			<ul>
+				{faqs.map(({ question, answer, category: { title } }, idx) =>
+					<li key={idx} className={cn(list[idx] && styles.selected)} onClick={()=> setList({...list, [idx]: list[idx] ? false : true})}>
+						<div className={styles.header}>
+							<h2 className={styles.question}>{question}</h2>	
+							<div className={styles.indicator}>+</div>
+						</div>
+						<Markdown className={styles.answer}>{answer}</Markdown>
+					</li>
+				)}
+			</ul>
 		</Section>
 	)
 }
