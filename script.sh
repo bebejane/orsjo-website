d8652cffe245721017cca3273c985193e8a65804
Bebe Jane <bjornthief@gmail.com>
2022-08-18 14:12:31 +0200

    add git cancel script

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
+  # Build
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+    echo "🛑 - Build cancelled"
+  exit 1;
+fi
