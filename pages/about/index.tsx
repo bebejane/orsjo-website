a5bffab7094270e55cb673ef94ff441fd4ecd424
konst-teknik <information-konst-teknik.se>
2022-08-23 11:10:48 +0200

    Typo

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,19 +4,20 @@ import { withGlobalProps } from "/lib/hoc";
 import Link from 'next/link'
 import { Image } from 'react-datocms'
 import Markdown from '/lib/dato/components/Markdown';
-import type {  PageLayoutProps } from '/lib/context/layout';
+import type { PageLayoutProps } from '/lib/context/layout';
 import { Section, VideoPlayer } from '/components'
+import cn from 'classnames'
 
-export type AboutProps = {  about: AboutRecord}
+export type AboutProps = { about: AboutRecord }
 
-export default function About({ about : {title, image, intro, sections} }: AboutProps) {
-	
+export default function About({ about: { title, image, intro, sections } }: AboutProps) {
+
 	return (
 		<>
 			<Section className={styles.about} type="full">
 				<div className={styles.hero}>
-					<Image 
-						data={image.responsiveImage} 
+					<Image
+						data={image.responsiveImage}
 						className={styles.heroImage}
 						objectFit="cover"
 					/>
@@ -29,23 +30,23 @@ export default function About({ about : {title, image, intro, sections} }: About
 				</Markdown>
 			</Section>
 			<Section className={styles.blocks} type="full">
-				{sections.map(({text, image, video}, idx) =>
+				{sections.map(({ text, image, video }, idx) =>
 					<div className={styles.block} key={idx}>
 						<div className={styles.left}>
-							<Markdown className={styles.text}>
+							<Markdown className={cn(styles.text, "large")}>
 								{text}
 							</Markdown>
 						</div>
 						<div className={styles.right}>
-							{image ? 
-								<Image data={image.responsiveImage} className={styles.image}/>
-							: video ?
-								<VideoPlayer
-									className={styles.video}
-									data={image}
-								/>
-							: 
-								null 
+							{image ?
+								<Image data={image.responsiveImage} className={styles.image} />
+								: video ?
+									<VideoPlayer
+										className={styles.video}
+										data={image}
+									/>
+									:
+									null
 							}
 						</div>
 					</div>
@@ -55,9 +56,9 @@ export default function About({ about : {title, image, intro, sections} }: About
 	)
 }
 
-About.layout = { layout:'full', color:"--black", menu: 'inverted'} as PageLayoutProps
+About.layout = { layout: 'full', color: "--black", menu: 'inverted' } as PageLayoutProps
 
-export const getStaticProps = withGlobalProps({ queries: [AboutDocument], model:'about' }, async ({ props, revalidate }: any) => {
+export const getStaticProps = withGlobalProps({ queries: [AboutDocument], model: 'about' }, async ({ props, revalidate }: any) => {
 
 	return {
 		props,
