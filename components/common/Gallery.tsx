20eb9d5f1dc5f1b1808e37030f5a47e3d4cc0f54
konst-teknik <information-konst-teknik.se>
2022-09-20 16:57:11 +0200

    Nytt

--- components/common/Gallery.tsx
+++ components/common/Gallery.tsx
@@ -4,42 +4,42 @@ import cn from 'classnames'
 import { Image } from "react-datocms"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState, useRef, useEffect, MouseEventHandler } from 'react';
-import type { Swiper as SwiperType} from 'swiper'
+import type { Swiper as SwiperType } from 'swiper'
 
 export type GalleryProps = {
-  images: FileField[], 
-  onClose: (event?: React.MouseEvent) => void, 
-  index:number, 
-  show:boolean
+  images: FileField[],
+  onClose: (event?: React.MouseEvent) => void,
+  index: number,
+  show: boolean
 }
 
-export default function Gallery({ images, onClose, index = 0, show } : GalleryProps) {
-  
+export default function Gallery({ images, onClose, index = 0, show }: GalleryProps) {
+
   const swiperRef = useRef<SwiperType | undefined>()
   const [realIndex, setRealIndex] = useState(index)
   const [title, setTitle] = useState<string>()
   const isSingleSlide = images?.length === 1
-  
+
   useEffect(() => images && setTitle(images[realIndex]?.title), [realIndex, images])
 
-  useEffect(()=> { // handle  keys
-    const handleKeys = ({key}) => {
-      if(key === 'ArrowRight') swiperRef?.current?.slideNext()
-      if(key === 'ArrowLeft') swiperRef?.current?.slidePrev()
-      if(key === 'Escape') onClose()
+  useEffect(() => { // handle  keys
+    const handleKeys = ({ key }) => {
+      if (key === 'ArrowRight') swiperRef?.current?.slideNext()
+      if (key === 'ArrowLeft') swiperRef?.current?.slidePrev()
+      if (key === 'Escape') onClose()
     }
     document.addEventListener('keydown', handleKeys)
     return () => document.removeEventListener('keydown', handleKeys)
   }, [onClose])
 
   if (!images || !show) return null
-  
+
   return (
     <div className={cn(styles.gallery, images.length <= 1 && styles.noArrows, isSingleSlide && styles.noArrows)}>
-      <div className={styles.back} onClick={() => swiperRef.current.slidePrev()}>❮</div>
+      <div className={styles.back} onClick={() => swiperRef.current.slidePrev()}><img src="/images/arrow.svg" className={styles.arrow} /></div>
       <div className={styles.images} onClick={() => !isSingleSlide && swiperRef?.current?.slideNext()}>
         <Swiper
-          id={`main-gallery`} 
+          id={`main-gallery`}
           loop={true}
           spaceBetween={500}
           slidesPerView={1}
@@ -49,7 +49,7 @@ export default function Gallery({ images, onClose, index = 0, show } : GalleryPr
         >
           {images.map((image, idx) =>
             <SwiperSlide key={idx} className={styles.slide}>
-              {image.responsiveImage ? 
+              {image.responsiveImage ?
                 <Image
                   className={styles.image}
                   pictureClassName={styles.picture}
@@ -57,7 +57,7 @@ export default function Gallery({ images, onClose, index = 0, show } : GalleryPr
                   lazyLoad={false}
                   usePlaceholder={false}
                 />
-              :
+                :
                 <div className={styles.svg}>
                   <img src={image.url} className={styles.image} />
                 </div>
@@ -66,7 +66,7 @@ export default function Gallery({ images, onClose, index = 0, show } : GalleryPr
           )}
         </Swiper>
       </div>
-      <div className={styles.forward} onClick={() => swiperRef.current.slideNext()}>❯</div>
+      <div className={styles.forward} onClick={() => swiperRef.current.slideNext()}><img src="/images/arrow.svg" className={styles.arrow} /></div>
       <div className={styles.caption}>{title}</div>
       <div className={styles.close} onClick={onClose}>×</div>
     </div>
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -32,7 +32,7 @@ export default function Contact({ contact, resellers, staffs, showrooms, distrib
 			resellesByCountry[r.country.id] = { resellers: [], country: r.country.name }
 		resellesByCountry[r.country.id].resellers.push(r)
 	})
-	
+
 	return (
 		<>
 			<Section name="Information" top={true} className={styles.informationSection} bgColor='--red'>
@@ -56,13 +56,13 @@ export default function Contact({ contact, resellers, staffs, showrooms, distrib
 							</p>
 						</div>
 					</div>
-					<button onClick={()=>setShowContactForm(true)}>Contact Us</button>
+					<button onClick={() => setShowContactForm(true)}>Contact Us</button>
 				</div>
 				<div className={styles.imageWrap}>
 					<Image data={contact.image.responsiveImage} className={styles.image} />
 				</div>
 			</Section>
-			
+
 			<Section name="Staff" className={styles.staffSection} bgColor='--red'>
 				<h1 className="bottomMargin">Staff</h1>
 				<div className={styles.staff}>
@@ -84,11 +84,11 @@ export default function Contact({ contact, resellers, staffs, showrooms, distrib
 						</div>
 					)}
 				</div>
-				{showContactForm && 
-					<ContactModal onClose={()=>setShowContactForm(false)}/>
+				{showContactForm &&
+					<ContactModal onClose={() => setShowContactForm(false)} />
 				}
 			</Section>
-			<Section name="Showrooms"  className={styles.showroomsSection} bgColor='--black'>
+			<Section name="Showrooms" className={styles.showroomsSection} bgColor='--black'>
 				<h1>Showrooms</h1>
 				<Markdown className={styles.intro}>
 					The best way to experience our products is to see them in real life, so just reach out and book an appointment.
@@ -166,24 +166,24 @@ export default function Contact({ contact, resellers, staffs, showrooms, distrib
 					})}
 				</div>
 			</Section>
-			
+
 		</>
 	)
 }
 
 
-const ContactModal = ({onClose}) => {
+const ContactModal = ({ onClose }) => {
 
 	const { register, handleSubmit } = useForm();
 	const [loading, setLoading] = useState(false)
 	const [success, setSuccess] = useState(false)
 	const [error, setError] = useState()
 
-  const [data, setData] = useState("");
+	const [data, setData] = useState("");
 
-	useEffect(()=>{ 
+	useEffect(() => {
 
-		if(!data) return 
+		if (!data) return
 
 		setLoading(true)
 
@@ -194,17 +194,17 @@ const ContactModal = ({onClose}) => {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(data)
-		}).then(()=>{	
+		}).then(() => {
 			setSuccess(true)
-		}).catch((err)=>{
+		}).catch((err) => {
 			setError(err)
-		}).finally(()=>{
+		}).finally(() => {
 			setLoading(false)
 		})
 
 	}, [data])
 
-	return(
+	return (
 		<Modal>
 			<div className={styles.contactForm}>
 				<div className={styles.wrap}>
@@ -213,17 +213,17 @@ const ContactModal = ({onClose}) => {
 						<label htmlFor="name">Name</label>
 						<input id="name" name="name" type="text" placeholder="Name..." {...register("name")} />
 						<label htmlFor="email">E-mail</label>
-						<input id="email" type="text" name="email" placeholder="E-mail..." {...register("email")}/>
+						<input id="email" type="text" name="email" placeholder="E-mail..." {...register("email")} />
 						<label htmlFor="subject">Subject</label>
-						<input id="subject" type="text" name="subject" placeholder="Subject..." {...register("subject")}/>
+						<input id="subject" type="text" name="subject" placeholder="Subject..." {...register("subject")} />
 						<label htmlFor="text">Message</label>
 						<textarea name="text" {...register("text")}></textarea>
 						<button type="submit">Send</button>
 					</form>
-					{loading && 
+					{loading &&
 						<div className={styles.loading}>Submitting...</div>
 					}
-					{success && 
+					{success &&
 						<div className={styles.success}>Success!</div>
 					}
 				</div>
@@ -234,7 +234,7 @@ const ContactModal = ({onClose}) => {
 }
 
 
-Contact.layout = { layout: 'normal', color: "--red", menu: 'inverted' } as PageLayoutProps
+Contact.layout = { layout: 'normal', color: "--red", menu: 'inverted', footerLine: true } as PageLayoutProps
 
 export const getStaticProps = withGlobalProps({ queries: [ContactDocument, AllResellersDocument, AllStaffsDocument, AllShowroomsDocument, AllDistributorsDocument] }, async ({ props, revalidate }: any) => {
 
