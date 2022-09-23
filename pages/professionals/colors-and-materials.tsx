439c805841ee7c54a012baa34dbbf2d3f94103e8
Bebe Jane <bjornthief@gmail.com>
2022-09-23 14:50:16 +0200

    colors and materials pages

--- pages/professionals/colors-and-materials.tsx
+++ pages/professionals/colors-and-materials.tsx
@@ -1,37 +1,39 @@
 import styles from './colors-and-materials.module.scss'
-//import {  } from '/graphql';
+import { AllColorsAndMaterialsDocument } from '/graphql';
 import withGlobalProps from "/lib/withGlobalProps";
 import Markdown from '/lib/dato/components/Markdown';
 import { PageProps } from '/lib/context/page';
 import { Thumbnail, Section } from '/components';
 
-export type ColorsAndMaterialsProps = {  }
+type ColorsAndMaterialsProps = { 
+	colorMaterials : ColorMaterialRecord[], 
+	colorMaterialTypes : ColorMaterialTypeRecord[],
+	colorMaterialIntro: ColorMaterialIntroRecord
+}
 
-export default function ColorsAndMaterials({  }: ColorsAndMaterialsProps) {
-	const title = 'Colors & Materials'
-	const intro = 'Intro...'
-	const projectTypes = []
-	const materials = []
+export default function ColorsAndMaterials({  colorMaterials, colorMaterialTypes, colorMaterialIntro:{ intro} }: ColorsAndMaterialsProps) {
+	
 	
 	return (
 		<>
 			<Section className={styles.introduction} top={true}>
-				<h1 className="topMargin">{title}</h1>
+				<h1 className="topMargin">Colors & Materials</h1>
 				<Markdown className={styles.intro}>
 					{intro}
 				</Markdown>
 			</Section>
-			{projectTypes.filter(({id}) => materials.find(({projectType}) => projectType.id === id)).map(({ title, titlePlural, id }, idx) => {
+			{colorMaterialTypes.filter(({id}) => colorMaterials.find(({category}) => category.id === id)).map(({ id, categoryPlural }, idx) => {
 				return (
-					<Section name={titlePlural} className={styles.materials} key={idx} >
-						<h1>{titlePlural}</h1>
-						{materials.filter(({ projectType }) => projectType.id === id).map((p, idx) =>
+					<Section name={categoryPlural} className={styles.materials} key={idx} >
+						<h1>{categoryPlural}</h1>
+						{colorMaterials.filter(({category}) => category.id === id).map(({image, description}, idx) =>
 							<Thumbnail
-								slug={'/'}
-								title={''}
-								image={null}
+								slug={'#'}
+								title={description}
+								image={image}
 								key={`t-${idx}`}
 								theme='mid'
+								type="material"
 								className={styles.material}
 							/>
 						)}
@@ -44,8 +46,8 @@ export default function ColorsAndMaterials({  }: ColorsAndMaterialsProps) {
 
 ColorsAndMaterials.page = { layout: 'normal', color: "--gray", menu: 'inverted' } as PageProps
 
-export const getStaticProps = withGlobalProps({ queries: [] }, async ({ props, revalidate }: any) => {
-
+export const getStaticProps = withGlobalProps({ queries: [AllColorsAndMaterialsDocument] }, async ({ props, revalidate }: any) => {
+	
 	return {
 		props,
 		revalidate
