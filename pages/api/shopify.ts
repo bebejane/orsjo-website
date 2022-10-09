ad2e50951127461038f7224172ccfdd8e04ecca4
Bebe Jane <bjornthief@gmail.com>
2022-10-09 11:50:03 +0200

    fix webhhok

--- pages/api/shopify.ts
+++ pages/api/shopify.ts
@@ -3,24 +3,29 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req : NextApiRequest, res: NextApiResponse) {
   
-  const client = buildClient({apiToken:process.env.DATOCMS_CMS_TOKEN})
-  const product = req.body
-  console.log(product)
+  try{
   
-  const record = await client.items.list({
-    filter:{
-      type:'product', 
-      fields: {
-        shopify_id: product.admin_graphql_api_id
+    const client = buildClient({apiToken:process.env.DATOCMS_CMS_TOKEN})
+    const product = req.body
+    
+    const record = (await client.items.list({
+      filter:{
+        type:'product', 
+        fields: {
+          shopify_id: {eq: product.admin_graphql_api_id}
+        }
       }
-    }
-  })
-  console.log(record)
-  if(!record)
-    return res.status(500).json({success:false, error:'record not found'})
+    }))[0]
+    
+    if(!record)
+      return res.status(500).json({success:false, error:'record not found'})
 
-  await client.items.update(record.id, {shopify_data:product})
-  
-  return res.status(200).json({success:true})
+    await client.items.update(record.id, {shopify_data:JSON.stringify(product)})
+    
+    return res.status(200).json({success:true})
+
+  } catch(err){
+    return res.status(500).json({success:false, error:err.toString()})
+  }
 
 }
