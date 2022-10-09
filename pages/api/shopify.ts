8abf9b12954ce309ec85df38d6159c426dbfa740
Bebe Jane <bjornthief@gmail.com>
2022-10-09 11:34:34 +0200

    webhook

--- pages/api/shopify.ts
+++ pages/api/shopify.ts
@@ -1,11 +1,25 @@
+import { buildClient } from '@datocms/cma-client-node'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req : NextApiRequest, res: NextApiResponse) {
-  console.log('shopify webhook')
-  const body = req.body
+export default async function handler(req : NextApiRequest, res: NextApiResponse) {
+  
+  const client = buildClient({apiToken:process.env.DATOCMS_CMS_TOKEN})
+  const product = JSON.parse(req.body)
 
-  console.log(body)
+  const record = await client.items.list({
+    filter:{
+      type:'product', 
+      fields: {
+        shopify_id: product.admin_graphql_api_id
+      }
+    }
+  })
+  console.log(record)
+  if(!record)
+    return res.status(500).json({success:false, error:'record not found'})
 
-  res.status(200).json({success:true})
+  await client.items.update(record.id, {shopify_data:product})
+  
+  return res.status(200).json({success:true})
 
 }
