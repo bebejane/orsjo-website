5c764d7e17adf1af0e41efd5dc9e726dccf50eaf
Bebe Jane <bjornthief@gmail.com>
2022-10-10 13:26:39 +0200

    dirty fix

--- components/common/AnchorLink.tsx
+++ components/common/AnchorLink.tsx
@@ -1,19 +1,35 @@
 import Link from "next/link";
+import { styleVariables } from "/lib/utils";
 import { useRouter } from "next/router";
+import { useWindowSize } from "rooks";
 
-export default function AnchorLink(props){
+export default function AnchorLink({href, children}){
 
   const router = useRouter()
+  const { innerWidth } = useWindowSize()
+
 
   const handleClick = (e) => {
     e.preventDefault()
-    const hash = props.href.split('#')[1]
-    //router.push('#' + props.href.split('#')[1], null, {shallow:true, scroll:false})
-    const el = window.document.getElementById(hash)
-    el.scrollIntoView({behavior:'smooth'})
+    const hash = href.split('#')[1]
+    const path = href.split('#')[0]
+    const currentPath = router.asPath.split('#')[0]
+
+    if(path === currentPath){
+      const el = window.document.getElementById(hash)
+      const { tablet, navbarHeightMobile, navbarHeight } = styleVariables;
+      const topMargin = (innerWidth < tablet ? navbarHeightMobile : navbarHeight) as number
+      const top = el ? (el.getBoundingClientRect().top + window.scrollY) - topMargin : 0
+      
+      window.scrollTo({ top, behavior:'smooth' })
+      
+      //el.scrollIntoView({behavior:'smooth'})
+    }else{
+      router.push(href)
+    }
     
   }
   //return <Link href={props.href} scroll={false}><a>{props.children}</a></Link>
-  return <a onClick={handleClick}>{props.children}</a>
+  return <a onClick={handleClick} style={{cursor:'pointer'}}>{children}</a>
 
 }
