ddf1de974b34075b41d5b0a7840f198b02427b55
Bebe Jane <bjornthief@gmail.com>
2022-10-12 12:26:12 +0200

    fix revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { buildClient } from '@datocms/cma-client-node';
 
 export const basicAuth = (req: NextApiRequest) => {
+  
   const basicAuth = req.headers.authorization
   if (!basicAuth) 
     return true;
@@ -18,7 +19,7 @@ const modelToPath = {
 
 const getPathsFromPayload = async (payload: any) => {
   const record = await getRecordFromPayload(payload)
-  const { apiKey } = record.model;
+  const { api_key : apiKey } = record.model;
   let slugs = []
   
   switch (apiKey) {
@@ -30,33 +31,45 @@ const getPathsFromPayload = async (payload: any) => {
       break;
     case 'designer':
       slugs.push(`/designers/${record.slug}`)
-    break;
+      break;
     case 'project':
       slugs.push(`/professionals/${record.slug}`)
       slugs.push(`/professionals`)
+      break;
     case 'bespoke':
       slugs.push(`/professionals/bespoke`)
+      break;
     case 'color_material':
       slugs.push(`/professionals/colors-and-materials`)
+      break;
     case 'about':
       slugs.push(`/about/about-us`)
+      break;
     case 'sustainability':
       slugs.push(`/about/sustainability`)
+      break;
     case 'press':
       slugs.push(`/about/press`)
+      break;
     case 'news':
       slugs.push(`/about/news`)
       slugs.push(`/`)
+      break;
     case 'job':
       slugs.push(`/about/jobs`)
+      break;
     case 'faq':
       slugs.push(`/support/faq`)
+      break;
     case 'faq_start':
       slugs.push(`/support/faq`)
+      break;
     case 'faq_category':
       slugs.push(`/support/faq`)
+      break;
     case 'contact': case 'staff': case 'showroom': case 'reseller': case 'distributor':
       slugs.push(`/contact`)
+      break;
     default:
       break;
   }
@@ -84,14 +97,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const paths = await getPathsFromPayload(payload)
 
     if (!paths.length)
-      throw new Error(`Nothing to revalidate`);
+      return console.log(`Nothing to revalidate`);
 
     console.log('revalidating paths', paths)
     await Promise.all(paths.map(path => res.revalidate(path)))
     console.log('revalidated done')
   } catch (err: any) {
     console.error(err)
-    res.status(500).send(`Error revalidating: ${err.message || err}`)
   }
 }
 
@@ -104,7 +116,7 @@ const getRecordFromPayload = async (payload: any) : Promise<any> => {
   const client = buildClient({ apiToken: process.env.NEXT_PUBLIC_GRAPHQL_API_TOKEN })
   const model = (await client.itemTypes.list()).filter(m => m.id === modelId)[0]
   const record = (await client.items.list({ filter: { type: model.api_key, fields: { id: { eq: payload.id } } } }))[0]
-
+  
   if (!record)
     throw `No record found with modelId: ${modelId}`
 
