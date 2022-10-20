31fc303dcd1d30fddc0d2c97d3b2831a2448b1b8
Bebe Jane <bjornthief@gmail.com>
2022-10-20 16:40:27 +0200

    har fixat

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -18,14 +18,15 @@ export default function Logo({inverted = false} : Props){
   const prevRoute = usePreviousRoute()
   const [characters, setCharacters] = useState(text.length)
   const { scrolledPosition, viewportHeight } = useScrollInfo()
-  const isStatic = prevRoute && router.asPath !== '/'
+  const isStatic = (prevRoute !== null && router.asPath !== '/')
 
   useEffect(()=>{
     const r = Math.min(1, scrolledPosition/(viewportHeight/2))
     const characters = text.length-Math.ceil(4*r)
     setCharacters(characters)
   }, [scrolledPosition, viewportHeight, setCharacters, prevRoute])
-
+  console.log(isStatic);
+  
   return(
     <Link scroll={false} href="/">
       <a className={styles.logo}>
