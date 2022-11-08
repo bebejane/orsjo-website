77553be37355ba9c48b2d0db72d9288826fbefc9
Bebe Jane <bjornthief@gmail.com>
2022-11-08 16:57:07 +0100

    gdpr

---
+++ components/common/CookieConsent.tsx
@@ -0,0 +1,45 @@
+import styles from './CookieConsent.module.scss'
+import cn from 'classnames'
+import { useState } from 'react'
+import { setCookie, deleteCookie, getCookie } from 'cookies-next';
+
+const GDPR_CONSENT_COOKIES = 'gdpr_consent_cookies'
+
+export default function CookieConsent() {
+
+  const [show, setShow] = useState(true)
+
+  const confirmConsent = (confirm: boolean) => {
+    if (confirm)
+      setCookie(GDPR_CONSENT_COOKIES, 'accepted', { maxAge: ((60 * 60) * 24 * 365) })
+    else
+      deleteCookie(GDPR_CONSENT_COOKIES)
+    setShow(false)
+  }
+
+  if (getCookie(GDPR_CONSENT_COOKIES) === 'accepted')
+    return null
+
+  return (
+    <>
+      <div className={cn(styles.options, show && styles.show)}>
+        <div className={styles.close} onClick={() => setShow(false)}>X</div>
+        <h1>Cookie policy</h1>
+        <p>
+          We use cookies and other tracking technologies to improve your browsing
+          experience on our site, show personalized content and targeted ads, analyze site
+          traffic, and understand where our audiences come from. To learn more or opt-out,
+          read our Cookie Policy. Please also read our Privacy Notice and Terms of Use,
+          which became effective December 20, 2019.
+        </p>
+        <p>
+          By choosing I Accept, you consent to our use of cookies and other tracking technologies.
+        </p>
+        <p>
+          <button onClick={() => confirmConsent(true)}> Accept all</button>
+          <button onClick={() => confirmConsent(false)}> Rejact all</button>
+        </p>
+      </div >
+    </>
+  )
+}
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import styles from './Layout.module.scss'
 import React, { useEffect } from 'react'
-import { Content, Sidebar, Footer, Gallery, SiteSearch, MenuDesktop, MenuMobile, Grid} from '/components'
+import { Content, Sidebar, Footer, Gallery, SiteSearch, MenuDesktop, MenuMobile, Grid, CookieConsent } from '/components'
 import { usePage } from '/lib/context/page'
 import type { MenuItem } from '/lib/menu'
 import { useStore, shallow } from '/lib/store'
@@ -10,23 +10,23 @@ import { useRouter } from 'next/router'
 
 export type LayoutProps = { children: React.ReactNode, menu: MenuItem[], title: string }
 
-export default function Layout({ children, menu : menuFromProps, title }: LayoutProps) {
+export default function Layout({ children, menu: menuFromProps, title }: LayoutProps) {
 
 	const { color, layout } = usePage()
 	const [gallery, setGallery, showSiteSearch, setShowSiteSearch] = useStore((state) => [state.gallery, state.setGallery, state.showSiteSearch, state.setShowSiteSearch], shallow)
 	const [menu, setMenu] = useState(menuFromProps)
 
-	useEffect(()=>{ // Refresh menu on load.
+	useEffect(() => { // Refresh menu on load.
 		buildMenu().then(res => setMenu(res)).catch(err => console.error(err))
 	}, [])
-	
+
 	return (
 		<>
-			<div className={styles.layout} style={{ backgroundColor: color || undefined }}>	
-				<MenuDesktop items={menu} onShowSiteSearch={()=>setShowSiteSearch(true)}/>
-				<MenuMobile items={menu}/>
-				<SiteSearch show={showSiteSearch} onClose={()=>setShowSiteSearch(false)}/>
-				<Sidebar title={title} show={layout !== 'full'}/>
+			<div className={styles.layout} style={{ backgroundColor: color || undefined }}>
+				<MenuDesktop items={menu} onShowSiteSearch={() => setShowSiteSearch(true)} />
+				<MenuMobile items={menu} />
+				<SiteSearch show={showSiteSearch} onClose={() => setShowSiteSearch(false)} />
+				<Sidebar title={title} show={layout !== 'full'} />
 				<Content>
 					{children}
 				</Content>
@@ -35,10 +35,11 @@ export default function Layout({ children, menu : menuFromProps, title }: Layout
 					images={gallery?.images}
 					index={gallery?.index}
 					padImagesWithTitle={gallery?.padImagesWithTitle}
-					onClose={() => setGallery({...gallery, index:-1})}
+					onClose={() => setGallery({ ...gallery, index: -1 })}
 				/>
 			</div>
 			<Footer menu={menu} />
+			<CookieConsent />
 			<Grid />
 		</>
 	)
