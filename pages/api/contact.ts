1a34c032aaf961ea7732c8ca07e497cd9a355c70
Bebe Jane <bjornthief@gmail.com>
2022-11-16 09:33:43 +0100

    update nodemailer config

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD,
       },
-      secure: false,
+      secure: true,
     })
 
     const mailData = {
