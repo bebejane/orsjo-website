9a72da22cda76e97dbad77479a6610ba66c56baa
Bebe Jane <bjornthief@gmail.com>
2022-12-28 11:55:33 +0100

    basic 404/500 pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,9 @@
-import styles from './404.module.scss'
-import cn from 'classnames'
+import s from './404.module.scss'
 import Link from 'next/link'
 
 export default function FourOhFour() {
   return (
-    <div className={styles.container}>
+    <div className={s.container}>
       <h1>404 - Page Not Found</h1>
       <Link prefetch={false} href="/">
         <a>
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,10 +1,10 @@
-import styles from './500.module.scss'
+import s from './404.module.scss'
 import Link from 'next/link'
 
 export default function FiveZeroZero() {
   return (
-    <div className={styles.container}>
-      <h1>404 - Page Not Found</h1>
+    <div style={s.container}>
+      <h1>500 - Server-side error occurred</h1>
       <Link prefetch={false} href="/">
         <a>
           Go back home
