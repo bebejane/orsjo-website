c7d18d850e7afbdad998b536a813336fc34767e2
Bebe Jane <bjornthief@gmail.com>
2025-06-12 17:22:56 +0200

    addons under selected

--- datocms.config.ts
+++ datocms.config.ts
@@ -2,22 +2,22 @@ import { DatoCmsConfig } from 'next-dato-utils/config';
 import client from './lib/client';
 
 const routes = {
-	about: async ({ id }) => ['/about'],
-	bespoke: async ({ id }) => ['/professionals/bespoke'],
-	catalogue: async ({ id }) => ['/professionals/downloads'],
-	color_material: async ({ id }) => ['/professionals/colors-and-materials'],
-	color_material_intro: async ({ id }) => ['/professionals/colors-and-materials'],
-	color_material_type: async ({ id }) => ['/professionals/colors-and-materials'],
-	contact: async ({ id }) => ['/contact'],
-	country: async ({ id }) => ['/contact'],
+	about: async () => ['/about'],
+	bespoke: async () => ['/professionals/bespoke'],
+	catalogue: async () => ['/professionals/downloads'],
+	color_material: async () => ['/professionals/colors-and-materials'],
+	color_material_intro: async () => ['/professionals/colors-and-materials'],
+	color_material_type: async () => ['/professionals/colors-and-materials'],
+	contact: async () => ['/contact'],
+	country: async () => ['/contact'],
 	designer: async ({ slug }) => [`/designers/${slug}`],
-	distributor: async ({ id }) => ['/contact'],
-	downloads_start: async ({ id }) => ['/professionals/downloads'],
-	factory_visit: async ({ id }) => ['/professionals/factory-visit'],
-	faq: async ({ id }) => ['/support/faq'],
-	faq_category: async ({ id }) => ['/support/faq'],
-	faq_start: async ({ id }) => ['/support/faq'],
-	job: async ({ id }) => ['/about/jobs'],
+	distributor: async () => ['/contact'],
+	downloads_start: async () => ['/professionals/downloads'],
+	factory_visit: async () => ['/professionals/factory-visit'],
+	faq: async () => ['/support/faq'],
+	faq_category: async () => ['/support/faq'],
+	faq_start: async () => ['/support/faq'],
+	job: async () => ['/about/jobs'],
 	manual: async () => ['/support/manuals'],
 	news: async ({ slug }) => [`/about/news/${slug}`, `/`],
 	product: async ({ slug }) => [
@@ -27,19 +27,19 @@ const routes = {
 		`/products`,
 		`/`,
 	],
-	product_accessory: async ({ id }) => productReferences(id),
-	product_category: async ({ id }) => productReferences(id),
-	product_color: async ({ id }) => productReferences(id),
-	product_connection: async ({ id }) => productReferences(id),
-	product_dimmable: async ({ id }) => productReferences(id),
-	product_electrical: async ({ id }) => productReferences(id),
-	product_family: async ({ id }) => productReferences(id),
-	product_feature: async ({ id }) => productReferences(id),
-	product_lightsource: async ({ id }) => productReferences(id),
-	product_material: async ({ id }) => productReferences(id),
-	product_model_name: async ({ id }) => productReferences(id),
-	product_mounting: async ({ id }) => productReferences(id),
-	product_socket: async ({ id }) => productReferences(id),
+	product_accessory: async ({ id }) => references(id),
+	product_category: async ({ id }) => references(id),
+	product_color: async ({ id }) => references(id),
+	product_connection: async ({ id }) => references(id),
+	product_dimmable: async ({ id }) => references(id),
+	product_electrical: async ({ id }) => references(id),
+	product_family: async ({ id }) => references(id),
+	product_feature: async ({ id }) => references(id),
+	product_lightsource: async ({ id }) => references(id),
+	product_material: async ({ id }) => references(id),
+	product_model_name: async ({ id }) => references(id),
+	product_mounting: async ({ id }) => references(id),
+	product_socket: async ({ id }) => references(id),
 	product_start: async ({ id }) => ['/products'],
 	project: async ({ slug }) => [`/professionals/projects/${slug}`, `/professionals/projects`, `/`],
 	project_start: async () => ['/professionals/projects'],
@@ -68,28 +68,6 @@ export default {
 	},
 	routes,
 } satisfies DatoCmsConfig;
-
-async function productReferences(itemId: string): Promise<string[]> {
-	if (!itemId) throw new Error('Missing reference: itemId');
-	const paths: string[] = [];
-	try {
-		const products = await client.items.references(itemId, { version: 'published', limit: 500 });
-
-		if (products.length) {
-			paths.push(`/products`);
-			paths.push(`/professionals/downloads`);
-			paths.push(`/support/manuals`);
-			paths.push(`/`);
-			paths.push.apply(
-				paths,
-				products.map((product) => `/products/${product.slug}`)
-			);
-		}
-	} catch (e) {
-		console.log('error', e.message);
-	}
-	return paths;
-}
 
 async function references(itemId: string, upload: boolean = false): Promise<string[]> {
 	if (!itemId) throw new Error('datocms.config: Missing reference: itemId');
