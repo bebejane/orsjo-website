2fb2afff8b514d83c367f205311173fa3aa2001a
konst-teknik <information@konst-teknik.se>
2025-11-26 10:05:07 +0100

    Contact form

--- app/[locale]/contact/message-us/page.tsx
+++ app/[locale]/contact/message-us/page.tsx
@@ -25,13 +25,9 @@ export default async function ContactModalPage({ params }: PageProps<'/[locale]/
 				<h1>Contact us</h1>
 				<div className={s.text}>
 					<p className='medium'>
-						<span>OBS!</span> Vi kan tyvärr inte erbjuda teknisk support till privatpersoner, vänligen kontakta någon av
-						våra <a href='https://www.orsjo.com/contact#retailers'>återförsäljare</a>. Vi har samlat de vanligaste
-						frågorna med svar på vår <a href='https://www.orsjo.com/support/faq'>FAQ-sida.</a>
+						Vi har samlat de vanligastefrågorna med svar på vår <a href='https://www.orsjo.com/support/faq'>FAQ-sida.</a>
 					</p>
-					<p className='medium'>
-						<span>NOTE!</span> We&apos;re not able to offer techical support for private customers, intead, please
-						contact one of our <a href='https://www.orsjo.com/contact#retailers'>retailers</a>. We&apos;ve collected the
+					<p className='medium'>We&apos;ve collected the
 						most common questions on our <a href='https://www.orsjo.com/support/faq'>FAQ page.</a>
 					</p>
 				</div>
