73876de49c9ef06647c98ab6cc4acdc9e5d04700
Bebe Jane <bjornthief@gmail.com>
2026-04-09 14:13:49 +0200

    Geins webshop

--- next.config.ts
+++ next.config.ts
@@ -90,8 +90,8 @@ const nextConfig: NextConfig = {
 	},
 };
 
-export default withNextIntl(nextConfig);
-/*
+//export default withNextIntl(nextConfig);
+
 export default withSentryConfig(withNextIntl(nextConfig), {
 	// For all available options, see:
 	// https://www.npmjs.com/package/@sentry/webpack-plugin#options
@@ -124,4 +124,3 @@ export default withSentryConfig(withNextIntl(nextConfig), {
 	// https://vercel.com/docs/cron-jobs
 	automaticVercelMonitors: true,
 });
-*/
