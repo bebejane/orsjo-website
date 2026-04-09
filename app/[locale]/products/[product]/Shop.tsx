e5f0ad3ef5db69389c7d64cc7ee254624b34da21
Bebe Jane <bjornthief@gmail.com>
2026-04-09 15:29:45 +0200

    fic currency

--- app/[locale]/products/[product]/Shop.tsx
+++ app/[locale]/products/[product]/Shop.tsx
@@ -183,6 +183,7 @@ export default function ProductShop({ product, geins, variantId, shipping, marke
 		resetAll();
 	}
 
+	console.log({ product, selected, selectedModel });
 	if (!product || !selected || !selectedModel) return null;
 
 	return (
