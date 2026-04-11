f2da0b10e15033b81e2c536f1f833d0dff0203aa
Bebe Jane <bjornthief@gmail.com>
2026-04-11 09:57:07 +0200

    fix config

--- datocms.config.ts
+++ datocms.config.ts
@@ -1,5 +1,6 @@
 import {
 	DatoCmsConfig,
+	getItemApiKey,
 	getItemReferenceRoutes,
 	getUploadReferenceRoutes,
 } from 'next-dato-utils/config';
@@ -8,40 +9,118 @@ import { defaultLocale, locales } from '@/i18n/routing';
 import { SitemapDocument } from '@/graphql';
 import { apiQuery } from 'next-dato-utils/api';
 
+export function getRoute(item: any, locale?: string | null): string {
+	const apiKey = getItemApiKey(item);
+	if (!apiKey) throw new Error('No api key found');
+	const { slug } = item;
+
+	switch (apiKey) {
+		case 'about':
+			return '/about';
+		case 'bespoke':
+			return '/professionals/bespoke';
+		case 'catalogue':
+			return '/professionals/downloads';
+		case 'color_material':
+		case 'color_material_intro':
+		case 'color_material_type':
+			return '/professionals/colors-and-materials';
+		case 'contact':
+		case 'country':
+		case 'distributor':
+		case 'reseller':
+		case 'showroom':
+		case 'staff':
+			return '/contact';
+		case 'designer':
+			return `/designers/${slug}`;
+		case 'downloads_start':
+			return '/professionals/downloads';
+		case 'factory_visit':
+			return '/professionals/factory-visit';
+		case 'faq':
+		case 'faq_category':
+		case 'faq_start':
+			return '/support/faq';
+		case 'term':
+		case 'term_category':
+		case 'term_start':
+			return '/support/terms';
+		case 'privacy_policy':
+			return '/support/privacy-policy';
+		case 'job':
+			return '/about/jobs';
+		case 'manual':
+			return '/support/manuals';
+		case 'news':
+			return `/about/news/${slug}`;
+		case 'product':
+			return `/products/${slug}`;
+		case 'product_accessory':
+		case 'product_category':
+		case 'product_color':
+		case 'product_connection':
+		case 'product_dimmable':
+		case 'product_electrical':
+		case 'product_family':
+		case 'product_feature':
+		case 'product_lightsource':
+		case 'product_material':
+		case 'product_model_name':
+		case 'product_mounting':
+		case 'product_socket':
+		case 'product_start':
+			return '/products';
+		case 'project':
+			return `/professionals/projects/${slug}`;
+		case 'project_start':
+		case 'project_type':
+			return '/professionals/projects';
+		case 'start':
+			return '/';
+		case 'sustainability':
+			return '/about/sustainability';
+		case 'shipping':
+			return '/products';
+	}
+	throw new Error(`Unknown api key: ${apiKey}`);
+}
+
 export default {
 	i18n: {
 		defaultLocale,
 		locales,
 	},
+	route: async (item, locale) => getRoute(item, locale),
 	routes: {
-		about: async () => ['/se/about'],
-		bespoke: async () => ['/se/professionals/bespoke'],
-		catalogue: async () => ['/se/professionals/downloads'],
-		color_material: async () => ['/se/professionals/colors-and-materials'],
-		color_material_intro: async () => ['/se/professionals/colors-and-materials'],
-		color_material_type: async () => ['/se/professionals/colors-and-materials'],
-		contact: async () => ['/se/contact'],
-		country: async () => ['/se/contact'],
-		designer: async ({ slug }) => [`/se/designers/${slug}`],
-		distributor: async () => ['/se/contact'],
-		downloads_start: async () => ['/se/professionals/downloads'],
-		factory_visit: async () => ['/se/professionals/factory-visit'],
-		faq: async () => ['/se/support/faq'],
-		faq_category: async () => ['/se/support/faq'],
-		faq_start: async () => ['/se/support/faq'],
-		term: async () => ['/se/support/terms'],
-		term_category: async () => ['/se/support/terms'],
-		term_start: async () => ['/se/support/terms'],
-		privacy_policy: async () => ['/se/support/privacy-policy'],
-		job: async () => ['/se/about/jobs'],
-		manual: async () => ['/se/support/manuals'],
-		news: async ({ slug }) => [`/se/about/news/${slug}`, `/se/`],
+		about: async () => ['/[locale]/about'],
+		bespoke: async () => ['/[locale]/professionals/bespoke'],
+		catalogue: async () => ['/[locale]/professionals/downloads'],
+		color_material: async () => ['/[locale]/professionals/colors-and-materials'],
+		color_material_intro: async () => ['/[locale]/professionals/colors-and-materials'],
+		color_material_type: async () => ['/[locale]/professionals/colors-and-materials'],
+		contact: async () => ['/[locale]/contact'],
+		country: async () => ['/[locale]/contact'],
+		designer: async ({ slug }) => [`/[locale]/designers/${slug}`],
+		distributor: async () => ['/[locale]/contact'],
+		downloads_start: async () => ['/[locale]/professionals/downloads'],
+		factory_visit: async () => ['/[locale]/professionals/factory-visit'],
+		faq: async () => ['/[locale]/support/faq'],
+		faq_category: async () => ['/[locale]/support/faq'],
+		faq_start: async () => ['/[locale]/support/faq'],
+		term: async () => ['/[locale]/support/terms'],
+		term_category: async () => ['/[locale]/support/terms'],
+		term_start: async () => ['/[locale]/support/terms'],
+		privacy_policy: async () => ['/[locale]/support/privacy-policy'],
+		job: async () => ['/[locale]/about/jobs'],
+		manual: async () => ['/[locale]/support/manuals'],
+		news: async ({ slug }) => [`/[locale]/about/news/${slug}`, `/[locale]/`],
 		product: async ({ slug }) => [
-			`/se/products/${slug}`,
-			`/se/professionals/downloads`,
-			`/se/support/manuals`,
-			`/se/products`,
-			`/se`,
+			`/[locale]/products/${slug}`,
+			`/[locale]/professionals/downloads`,
+			`/[locale]/support/manuals`,
+			`/[locale]/products`,
+			`/[locale]`,
 		],
 		product_accessory: async ({ id }) => getItemReferenceRoutes(id),
 		product_category: async ({ id }) => getItemReferenceRoutes(id),
@@ -56,20 +135,20 @@ export default {
 		product_model_name: async ({ id }) => getItemReferenceRoutes(id),
 		product_mounting: async ({ id }) => getItemReferenceRoutes(id),
 		product_socket: async ({ id }) => getItemReferenceRoutes(id),
-		product_start: async ({ id }) => ['/se/products'],
+		product_start: async ({ id }) => ['/[locale]/products'],
 		project: async ({ slug }) => [
-			`/se/professionals/projects/${slug}`,
-			`/se/professionals/projects`,
-			`/se/`,
+			`/[locale]/professionals/projects/${slug}`,
+			`/[locale]/professionals/projects`,
+			`/[locale]/`,
 		],
-		project_start: async () => ['/se/professionals/projects'],
-		project_type: async () => ['/se/professionals/projects'],
-		reseller: async () => ['/se/contact'],
-		showroom: async () => ['/se/contact'],
-		staff: async () => [`/se/contact`],
-		start: async () => [`/se/`],
-		sustainability: async () => ['/se/about/sustainability'],
-		shipping: async () => ['/se/products'],
+		project_start: async () => ['/[locale]/professionals/projects'],
+		project_type: async () => ['/[locale]/professionals/projects'],
+		reseller: async () => ['/[locale]/contact'],
+		showroom: async () => ['/[locale]/contact'],
+		staff: async () => [`/[locale]/contact`],
+		start: async () => [`/[locale]/`],
+		sustainability: async () => ['/[locale]/about/sustainability'],
+		shipping: async () => ['/[locale]/products'],
 		upload: async ({ id }) => getUploadReferenceRoutes(id),
 	},
 	sitemap: async () => {
