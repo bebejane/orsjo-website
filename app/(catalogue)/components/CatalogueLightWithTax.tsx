37472b7b9ab47babb7c2cd7d9d4c1adfa1a7cb29
Bebe Jane <bjornthief@gmail.com>
2026-05-11 15:09:40 +0200

    light with tax moms

--- app/(catalogue)/components/CatalogueLightWithTax.tsx
+++ app/(catalogue)/components/CatalogueLightWithTax.tsx
@@ -16,14 +16,22 @@ export default function CatalogueLightWithTax({
 			{products.map((product, rowIdx) => (
 				<ProductRow
 					key={rowIdx}
+					withLightsource={withLightsource}
 					product={{
 						...product,
 						models: product.models.map((m) => ({
 							...m,
 							variants: m.variants.map((v) => ({ ...v, price: v.price * taxRate })),
+							lightsources: m.lightsources.map((l) => ({
+								...l,
+								lightsource: { ...l.lightsource, price: l.lightsource.price * taxRate },
+							})),
+							accessories: m.accessories.map((a) => ({
+								...a,
+								accessory: { ...a.accessory, price: a.accessory?.price ?? 0 * taxRate } as any,
+							})),
 						})),
 					}}
-					withLightsource={withLightsource}
 				/>
 			))}
 		</>
